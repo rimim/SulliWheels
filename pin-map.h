95be0e16065cad9a25a2367ba7749b540e2e685f
rimim <nomail@thanks.com>
2025-02-01 09:36:06 -0500

    Initial checkin

---
+++ pin-map.h
@@ -0,0 +1,6 @@
+#define NEOPIXEL_PIN 	   	4
+#define NEOPIXEL_COUNT  	8
+#define NEOPIXEL_BRIGHTNESS 50 // Set BRIGHTNESS to about 1/5 (max = 255)
+
+#define PIN_CAN1_RX         0
+#define PIN_CAN1_TX         1
